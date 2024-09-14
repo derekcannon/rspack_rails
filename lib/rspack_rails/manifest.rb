42657a51e2f86bd2db8d8bf0bdb4d35e75842091
Derek Cannon <derektcannon@gmail.com>
2024-09-13 21:45:49 -0700

    Make thread safe

--- lib/rspack_rails/manifest.rb
+++ lib/rspack_rails/manifest.rb
@@ -1,15 +1,19 @@
 module RspackRails
     class Manifest
+        @mutex = Mutex.new
+
         def self.load
-            manifest_path = Rails.root.join('public', 'packs', 'manifest.json')
-            mtime = File.mtime(manifest_path)
-      
-            if @manifest.nil? || @manifest_mtime != mtime
-              @manifest = JSON.parse(File.read(manifest_path))
-              @manifest_mtime = mtime
+            @mutex.synchronize do
+                manifest_path = Rails.root.join('public', 'packs', 'manifest.json')
+                mtime = File.mtime(manifest_path)
+        
+                if @manifest.nil? || @manifest_mtime != mtime
+                  @manifest = JSON.parse(File.read(manifest_path))
+                  @manifest_mtime = mtime
+                end
+        
+                @manifest
             end
-      
-            @manifest
         end
   
         def self.lookup(name)
