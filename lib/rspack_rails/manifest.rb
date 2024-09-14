1f4a3f88b0946824bb5bd473b728e0266cc94df7
Derek Cannon <derektcannon@gmail.com>
2024-09-13 21:43:07 -0700

    Reload manifest if there’s a change

--- lib/rspack_rails/manifest.rb
+++ lib/rspack_rails/manifest.rb
@@ -1,11 +1,19 @@
 module RspackRails
     class Manifest
-      def self.load
-        @manifest ||= JSON.parse(File.read(Rails.root.join('public', 'packs', 'manifest.json')))
-      end
+        def self.load
+            manifest_path = Rails.root.join('public', 'packs', 'manifest.json')
+            mtime = File.mtime(manifest_path)
+      
+            if @manifest.nil? || @manifest_mtime != mtime
+              @manifest = JSON.parse(File.read(manifest_path))
+              @manifest_mtime = mtime
+            end
+      
+            @manifest
+        end
   
-      def self.lookup(name)
-        load[name] || raise("Could not find asset #{name} in manifest")
-      end
+        def self.lookup(name)
+            load[name] || raise("Could not find asset #{name} in manifest")
+        end
     end
 end
